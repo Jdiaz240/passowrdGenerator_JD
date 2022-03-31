8baed47009f193a5b239d546b58dca89c505265d
Joshua Diaz <jad24096@gmail.com>
2022-03-31 18:05:08 -0400

    added required prompts.

--- script.js
+++ script.js
@@ -2,12 +2,44 @@
 var generateBtn = document.querySelector("#generate");
 
 const specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "=", "+", "(", ")", "{", "}", "[", "]", "?", "/", ">", "<", ".", ",", "~", "|"];
-​
+
 const numberChars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-​
+
 const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-​
+
 const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+function generatePassword() {
+  var firstprompt = prompt("How many characters would you like your password?")
+    if (firstprompt =  )
+
+  var secondprompt = prompt("Would you like to include numbers?")
+
+  var thirdprompt = prompt("Would you like to include special characters?")
+
+}
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -19,6 +51,6 @@ function writePassword() {
 }
 
   
-}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
